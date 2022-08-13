313b6342666d351390219d3f035ae11c79389e1a
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-13 16:36:59 -0400

    Fix title size

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 </script>
 
-<div class="flex border-2 border-neutral-header rounded-md p-3 justify-between max-w-2xl mx-auto w-full">
+<header class="flex border-2 border-neutral-header rounded-md p-3 justify-between max-w-2xl mx-auto w-full">
   <div class="flex items-center">
-    <h1 class="uppercase leading-[0.85] text-[min(3.15vw,1.5rem)]">
+    <h1 class="uppercase leading-[0.85] text-[min(3.25vw,1.5rem)]">
       Rock<br />
       Paper<br />
       Scissors<br />
@@ -16,4 +16,4 @@
     >
     <span class="text-neutral-dark text-[min(10vw,4rem)] font-bold leading-none">12</span>
   </div>
-</div>
+</header>
