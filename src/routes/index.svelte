5cc67f0dfd3921dad945b8efe8c6e202faddc948
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-14 00:31:20 -0400

    Conditionnal displaying of Pentagon/GameBoard

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
+  import { scale, blur } from "svelte/transition";
+  import { gameboard } from "$lib/stores";
   import Pentagon from "$lib/components/Pentagon.svelte";
+  import GameBoard from "$lib/components/GameBoard.svelte";
 </script>
 
 <svelte:head>
   <title>Frontend Mentor | Rock, Paper, Scissors, Lizard, Spock</title>
 </svelte:head>
 
-<main class="py-20 my-auto flex">
-  <div class="mx-auto max-w-[80%]">
-    <Pentagon />
-  </div>
+<main class="py-16 my-auto flex">
+  {#if $gameboard.status === "idle"}
+    <div in:blur={{ duration: 300 }} class="mx-auto max-w-[85%]">
+      <Pentagon />
+    </div>
+  {:else}
+    <div in:scale={{ duration: 300 }} class="w-full">
+      <GameBoard />
+    </div>
+  {/if}
 </main>
