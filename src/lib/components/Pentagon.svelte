1b4066d818c43bc5b14464cf8507a9249ba9b907
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-14 01:45:41 -0400

    Use divs w/ tabIndex='0' instead of buttons for w3c validation

--- src/lib/components/Pentagon.svelte
+++ src/lib/components/Pentagon.svelte
@@ -7,7 +7,6 @@
   <img src="/images/bg-pentagon.svg" alt="" />
   <div
     tabindex={$rulesOpen ? -1 : 0}
-    aria-label="Start round with scissors"
     on:click={() => startGame("scissors")}
     class="absolute top-4 left-1/2 -translate-x-1/2 -translate-y-1/2 hover:scale-105 origin-center transition-transform duration-150 cursor-pointer"
   >
@@ -15,7 +14,6 @@
   </div>
   <div
     tabindex={$rulesOpen ? -1 : 0}
-    aria-label="Start round with paper"
     on:click={() => startGame("paper")}
     class="absolute right-4 top-[22%] translate-x-1/2 hover:scale-105 origin-center transition-transform duration-150 cursor-pointer"
   >
@@ -23,7 +21,6 @@
   </div>
   <div
     tabindex={$rulesOpen ? -1 : 0}
-    aria-label="Start round with rock"
     on:click={() => startGame("rock")}
     class="absolute bottom-4 right-[5%] translate-y-1/2 hover:scale-105 origin-center transition-transform duration-150 cursor-pointer"
   >
@@ -31,7 +28,6 @@
   </div>
   <div
     tabindex={$rulesOpen ? -1 : 0}
-    aria-label="Start round with lizard"
     on:click={() => startGame("lizard")}
     class="absolute bottom-4 left-[5%] translate-y-1/2 hover:scale-105 origin-center transition-transform duration-150 cursor-pointer"
   >
@@ -39,7 +35,6 @@
   </div>
   <div
     tabindex={$rulesOpen ? -1 : 0}
-    aria-label="Start round with spock"
     on:click={() => startGame("spock")}
     class="absolute left-4 top-[22%] -translate-x-1/2 hover:scale-105 origin-center transition-transform duration-150 cursor-pointer"
   >
