93bbca90f112b9269561efa9f433f206a0f5ba7b
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-14 01:42:49 -0400

    Use divs w/ tabIndex='0' instead of buttons for w3c validation

--- src/lib/components/Pentagon.svelte
+++ src/lib/components/Pentagon.svelte
@@ -5,49 +5,44 @@
 
 <div class="relative w-full text-[min(4vw,1.25rem)]">
   <img src="/images/bg-pentagon.svg" alt="" />
-  <button
-    type="button"
+  <div
+    tabindex={$rulesOpen ? -1 : 0}
     aria-label="Start round with scissors"
-    disabled={$rulesOpen}
     on:click={() => startGame("scissors")}
-    class="absolute top-4 left-1/2 -translate-x-1/2 -translate-y-1/2 hover:scale-105 origin-center transition-transform duration-150"
+    class="absolute top-4 left-1/2 -translate-x-1/2 -translate-y-1/2 hover:scale-105 origin-center transition-transform duration-150 cursor-pointer"
   >
     <GameCard name="scissors" />
-  </button>
-  <button
-    type="button"
+  </div>
+  <div
+    tabindex={$rulesOpen ? -1 : 0}
     aria-label="Start round with paper"
-    disabled={$rulesOpen}
     on:click={() => startGame("paper")}
-    class="absolute right-4 top-[22%] translate-x-1/2 hover:scale-105 origin-center transition-transform duration-150"
+    class="absolute right-4 top-[22%] translate-x-1/2 hover:scale-105 origin-center transition-transform duration-150 cursor-pointer"
   >
     <GameCard name="paper" />
-  </button>
-  <button
-    type="button"
+  </div>
+  <div
+    tabindex={$rulesOpen ? -1 : 0}
     aria-label="Start round with rock"
-    disabled={$rulesOpen}
     on:click={() => startGame("rock")}
-    class="absolute bottom-4 right-[5%] translate-y-1/2 hover:scale-105 origin-center transition-transform duration-150"
+    class="absolute bottom-4 right-[5%] translate-y-1/2 hover:scale-105 origin-center transition-transform duration-150 cursor-pointer"
   >
     <GameCard name="rock" />
-  </button>
-  <button
-    type="button"
+  </div>
+  <div
+    tabindex={$rulesOpen ? -1 : 0}
     aria-label="Start round with lizard"
-    disabled={$rulesOpen}
     on:click={() => startGame("lizard")}
-    class="absolute bottom-4 left-[5%] translate-y-1/2 hover:scale-105 origin-center transition-transform duration-150"
+    class="absolute bottom-4 left-[5%] translate-y-1/2 hover:scale-105 origin-center transition-transform duration-150 cursor-pointer"
   >
     <GameCard name="lizard" />
-  </button>
-  <button
-    type="button"
+  </div>
+  <div
+    tabindex={$rulesOpen ? -1 : 0}
     aria-label="Start round with spock"
-    disabled={$rulesOpen}
     on:click={() => startGame("spock")}
-    class="absolute left-4 top-[22%] -translate-x-1/2 hover:scale-105 origin-center transition-transform duration-150"
+    class="absolute left-4 top-[22%] -translate-x-1/2 hover:scale-105 origin-center transition-transform duration-150 cursor-pointer"
   >
     <GameCard name="spock" />
-  </button>
+  </div>
 </div>
