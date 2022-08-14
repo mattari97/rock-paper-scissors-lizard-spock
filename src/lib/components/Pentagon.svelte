580078b00376e196a41445f898608183decd8b5d
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-13 21:10:55 -0400

    Add startGame handler

--- src/lib/components/Pentagon.svelte
+++ src/lib/components/Pentagon.svelte
@@ -1,22 +1,48 @@
 <script lang="ts">
+  import { startGame } from "$lib/stores";
   import GameCard from "$lib/components/GameCard.svelte";
 </script>
 
 <div class="relative w-full text-[min(3.75vw,1.25rem)]">
   <img src="/images/bg-pentagon.svg" alt="" />
-  <div class="absolute top-4 left-1/2 -translate-x-1/2 -translate-y-1/2">
+  <button
+    type="button"
+    aria-label="Start round with scissors"
+    on:click={() => startGame("scissors")}
+    class="absolute top-4 left-1/2 -translate-x-1/2 -translate-y-1/2 hover:scale-105 origin-center transition-transform duration-150"
+  >
     <GameCard name="scissors" />
-  </div>
-  <div class="absolute right-4 top-[22%] translate-x-1/2">
+  </button>
+  <button
+    type="button"
+    aria-label="Start round with paper"
+    on:click={() => startGame("paper")}
+    class="absolute right-4 top-[22%] translate-x-1/2 hover:scale-105 origin-center transition-transform duration-150"
+  >
     <GameCard name="paper" />
-  </div>
-  <div class="absolute bottom-4 right-[5%] translate-y-1/2">
+  </button>
+  <button
+    type="button"
+    aria-label="Start round with rock"
+    on:click={() => startGame("rock")}
+    class="absolute bottom-4 right-[5%] translate-y-1/2 hover:scale-105 origin-center transition-transform duration-150"
+  >
     <GameCard name="rock" />
-  </div>
-  <div class="absolute bottom-4 left-[5%] translate-y-1/2">
+  </button>
+  <button
+    type="button"
+    aria-label="Start round with lizard"
+    on:click={() => startGame("lizard")}
+    class="absolute bottom-4 left-[5%] translate-y-1/2 hover:scale-105 origin-center transition-transform duration-150"
+  >
     <GameCard name="lizard" />
-  </div>
-  <div class="absolute left-4 top-[22%] -translate-x-1/2">
+  </button>
+  <button
+    type="button"
+    aria-label="Start round with spock"
+    on:click={() => startGame("spock")}
+    class="absolute left-4 top-[22%] -translate-x-1/2 hover:scale-105 origin-center transition-transform duration-150"
+  >
     <GameCard name="spock" />
-  </div>
+  </button>
 </div>
